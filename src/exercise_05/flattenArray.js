95a21a90a54643e094ee1d347f797e5a8dfb1cd8
Your Name <you@example.com>
2020-09-07 22:32:43 +0800

    test-flattenArray

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,22 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  var res=[];
 
-  throw new Error('Please delete this line and implement the function');
+  if(array === null || array === undefined){
+    throw new Error('Flatten undefined or null array');
+  }else{
+    
+    for(var i = 0; i < array.length; i++){
+      if(Array.isArray(array[i])){
+        res =[].concat(...array);
+        //res = res.concat(flatten(array[i]));
+      }else{
+        res.push(array[i]);
+      } 
+    } 
+    
+    
+    return res; 
+  }
 }
